31642030bbc33c0380e3ee37838c0e5f144dc03c
Alumno <danigalcol@gmail.com>
2023-03-14 12:08:47 +0100

    Estructura y 3 atributos creados (Commit 5)

---
+++ Empresa.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Empresa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Empresa
+{
+    // instance variables - replace the example below with your own
+    private String nombreEmpresa;
+    private boolean abierto;
+    private String Direccion;
+
+    /**
+     * Constructor for objects of class Empresa
+     */
+    public Empresa()
+    {
+        
+
+    }
+
+    
+}
